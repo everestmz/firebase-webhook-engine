d6c74494f7ece540b731e913ca4c36bcd423b50c
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2016-09-18 08:56:51 -0400

    Cleanup

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-ar Firebase = require('firebase'),
-    usersRef = new Firebase('https://koan-cbd22.firebaseio.com/detailedprofiles/');
+var Firebase = require('firebase'),
+    fbf = new Firebase('your-firebase-url');
     http = require('http');
 var requestify = require('requestify');
 var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-
-usersRef.on('child_added', function(snapshot) {
-  requestify.post('localhost:3000/webhooks/firebase', snapshot.val())
-  //http.post({ host: 'koan-htn.herokuapp.com', path: '/webhooks/firebase' })
+fb.on('child_added', function(snapshot) {
+  requestify.post('your-post-url', snapshot.val())
 });
 
-var server = app.listen(3001, function() {
+var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
